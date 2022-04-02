3a99e8b703e6c62cd7bd494459a35d6c2341d293
[sachinimalsha] <[mhsm.dulakshi@gmail.com]>
2022-04-02 10:23:52 +0530

    new3

--- routes/web.php
+++ routes/web.php
@@ -74,66 +74,7 @@ Route::group(['middleware'=>['AuthCheck']],function(){
     Route::get('/admin/mountainclimb/mountainclimb_levels/mountainclimb_history',[UserAuthenticationController::class, 'mountainclimb_history']);
     Route::get('/admin/mountainclimb/mountainclimb_levels/mountainclimb_ict',[UserAuthenticationController::class, 'mountainclimb_ict']);
     Route::get('/admin/mountainclimb/mountainclimb_levels/mountainclimb_random',[UserAuthenticationController::class, 'mountainclimb_random']);
-
-    /*game levels-biology*/
-    //balloon shoot
-    Route::get('/admin/balloonshoot/balloonshoot_biology_levels/balloonshoot_biology_level1',[UserAuthenticationController::class, 'balloonshoot_biology_level1']);
-    //dart game
-    Route::get('/admin/dartgame/dartgame_biology_levels/dartgame_biology_level1',[UserAuthenticationController::class, 'dartgame_biology_level1']);
-    //mountain climb
-    Route::get('/admin/mountainclimb/mountainclimb_biology_levels/mountainclimb_biology_level1',[UserAuthenticationController::class, 'mountainclimb_biology_level1']);
-    //spin wheel
-    Route::get('/admin/spinwheel/spinwheel_biology_levels/spinwheel_biology_level1',[UserAuthenticationController::class, 'spinwheel_biology_level1']);
     
-    /*game levels-math*/
-    //balloon shoot
-    Route::get('/admin/balloonshoot/balloonshoot_math_levels/balloonshoot_math_level1',[UserAuthenticationController::class, 'balloonshoot_math_level1']);
-    //dart game
-    Route::get('/admin/dartgame/dartgame_math_levels/dartgame_math_level1',[UserAuthenticationController::class, 'dartgame_math_level1']);
-    //mountain climb
-    Route::get('/admin/mountainclimb/mountainclimb_math_levels/mountainclimb_math_level1',[UserAuthenticationController::class, 'mountainclimb_math_level1']);
-    //spin wheel
-    Route::get('/admin/spinwheel/spinwheel_math_levels/spinwheel_math_level1',[UserAuthenticationController::class, 'spinwheel_math_level1']);
-    
-    /*game levels-history*/
-    //balloon shoot
-    Route::get('/admin/balloonshoot/balloonshoot_history_levels/balloonshoot_history_level1',[UserAuthenticationController::class, 'balloonshoot_history_level1']);
-    //dart game
-    Route::get('/admin/dartgame/dartgame_history_levels/dartgame_history_level1',[UserAuthenticationController::class, 'dartgame_history_level1']);
-    //mountain climb
-    Route::get('/admin/mountainclimb/mountainclimb_history_levels/mountainclimb_history_level1',[UserAuthenticationController::class, 'mountainclimb_history_level1']);
-    //spin wheel
-    Route::get('/admin/spinwheel/spinwheel_history_levels/spinwheel_history_level1',[UserAuthenticationController::class, 'spinwheel_history_level1']);
-    
-    /*game levels-nature*/
-    //balloon shoot
-    Route::get('/admin/balloonshoot/balloonshoot_nature_levels/balloonshoot_nature_level1',[UserAuthenticationController::class, 'balloonshoot_nature_level1']);
-    //dart game
-    Route::get('/admin/dartgame/dartgame_nature_levels/dartgame_nature_level1',[UserAuthenticationController::class, 'dartgame_nature_level1']);
-    //mountain climb
-    Route::get('/admin/mountainclimb/mountainclimb_nature_levels/mountainclimb_nature_level1',[UserAuthenticationController::class, 'mountainclimb_nature_level1']);
-    //spin wheel
-    Route::get('/admin/spinwheel/spinwheel_nature_levels/spinwheel_nature_level1',[UserAuthenticationController::class, 'spinwheel_nature_level1']);
-
-    /*game levels-ict*/
-    //balloon shoot
-    Route::get('/admin/balloonshoot/balloonshoot_ict_levels/balloonshoot_ict_level1',[UserAuthenticationController::class, 'balloonshoot_ict_level1']);
-    //dart game
-    Route::get('/admin/dartgame/dartgame_ict_levels/dartgame_ict_level1',[UserAuthenticationController::class, 'dartgame_ict_level1']);
-    //mountain climb
-    Route::get('/admin/mountainclimb/mountainclimb_ict_levels/mountainclimb_ict_level1',[UserAuthenticationController::class, 'mountainclimb_ict_level1']);
-    //spin wheel
-    Route::get('/admin/spinwheel/spinwheel_ict_levels/spinwheel_ict_level1',[UserAuthenticationController::class, 'spinwheel_ict_level1']);
-
-    /*game levels-random*/
-    //balloon shoot
-    Route::get('/admin/balloonshoot/balloonshoot_random_levels/balloonshoot_random_level1',[UserAuthenticationController::class, 'balloonshoot_random_level1']);
-    //dart game
-    Route::get('/admin/dartgame/dartgame_random_levels/dartgame_random_level1',[UserAuthenticationController::class, 'dartgame_random_level1']);
-    //mountain climb
-    Route::get('/admin/mountainclimb/mountainclimb_random_levels/mountainclimb_random_level1',[UserAuthenticationController::class, 'mountainclimb_random_level1']);
-    //spin wheel
-    Route::get('/admin/spinwheel/spinwheel_random_levels/spinwheel_random_level1',[UserAuthenticationController::class, 'spinwheel_random_level1']);
 });
 
 
